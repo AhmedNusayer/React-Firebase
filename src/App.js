42a0d609d5d3a885e3bb76ff1469dcdcc01f95b4
Ahmed Nusayer Ashik <42416199+AhmedNusayer@users.noreply.github.com>
2021-01-07 17:58:49 +0600

    implemented login and basic route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import UpdateProfile from "./components/UpdateProfile";
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={SignUp} />
+      <Route path="/update-profile" exact component={UpdateProfile} />
+    </BrowserRouter>
   );
 }
 
